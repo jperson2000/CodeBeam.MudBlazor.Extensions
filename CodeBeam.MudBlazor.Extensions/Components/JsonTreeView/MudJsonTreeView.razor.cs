63f3078f33ccb3511365efd7fc55ae44970466bc
Jon Person <j.person@dodmarrow.org>
2024-04-14 19:42:56 -0600

    MudJsonTreeView: Uses a MudTreeView to view JSON

---
+++ CodeBeam.MudBlazor.Extensions/Components/JsonTreeView/MudJsonTreeView.razor.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Text.Json.Nodes;
+
+namespace MudExtensions;
+
+/// <summary>
+/// Represents a tree view which displays a snippet of JSON.
+/// </summary>
+public partial class MudJsonTreeView : ComponentBase
+{
+    /// <summary>
+    /// Gets or sets the JSON to be displayed.
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    public string Json { get; set; }
+
+    /// <summary>
+    /// Gets or sets the root node of the JSON to display.
+    /// </summary>
+    public JsonNode Root { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the tree contents are compacted.
+    /// </summary>
+    [Parameter]
+    public bool Dense { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the current row is highlighted.
+    /// </summary>
+    [Parameter]
+    public bool Hover { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether items are sorted by key.
+    /// </summary>
+    [Parameter]
+    public bool Sorted { get; set; }
+
+    /// <inheritdoc />
+    protected override void OnInitialized()
+    {
+        Root = JsonNode.Parse(Json);
+    }
+}
---
+++ CodeBeam.MudBlazor.Extensions/Components/JsonTreeView/MudJsonTreeViewNode.razor.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Text.Json.Nodes;
+
+namespace MudExtensions;
+
+/// <summary>
+/// Represents the child leaf of a JSON tree view.
+/// </summary>
+public partial class MudJsonTreeViewNode : ComponentBase
+{
+    /// <summary>
+    /// Gets or sets the node to display (including children).
+    /// </summary>
+    [Parameter]
+    [EditorRequired]
+    public JsonNode Node { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether items are sorted by key.
+    /// </summary>
+    [Parameter]
+    public bool Sorted { get; set; }
+}
